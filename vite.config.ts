d7acb619cfaab8518c79475c10b1400e9ad961f9
tn <2825557671@qq.com>
2025-03-12 21:00:16 +0800

    init2

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/todolist-app/',
 })
 
-
-
-// vue.config.ts
-module.exports = {
-  publicPath: './',
-}
-// vite.config.ts
-base: './'
