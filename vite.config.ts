33ea49d2d431c67d7c8556fabfd185937983340d
tn <2825557671@qq.com>
2025-03-12 21:07:39 +0800

    init4

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   base: '/todolist-app/',
+  build: {
+    outDir: 'docs',
+    assetsDir: 'assets',
+    sourcemap: false,
+    minify: 'terser',
+  }
 })
 
